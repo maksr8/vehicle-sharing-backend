cf00d8d6a71df061d86d26328162d3d13048f0e9
maksr8 <158086853+maksr8@users.noreply.github.com>
2026-04-02 23:55:07 +0300

    Fix cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express, { type Request, type Response } from "express";
 import { routes } from "./routes/index.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { notFoundHandler } from "./middleware/notFound.js";
+import cors from "cors";
 
 export const app = express();
+
+app.use(cors());
 
 app.get("/", (_req: Request, res: Response) => {
   res.json({ message: "Vehicle Sharing API" });
